1174ed0fee1fa480f575ab75e54cd657bc9e1aba
刘松森 <liusongsen@liusongsendeMacBook-Pro.local>
2019-07-30 16:18:21 +0800

    增加测试用例

--- test/test_iterator.php
+++ test/test_iterator.php
@@ -1,16 +1,14 @@
 <?php
+include "../vendor/autoload.php";
 
-namespace App\Design\Test;
 
-use  App\Design\Iterator;
-
-$bookShelf = new Iterator\BookShelf(3);
-$bookShelf->appendBook(new Iterator\Book("book1"));
-$bookShelf->appendBook(new Iterator\Book("book2"));
-$bookShelf->appendBook(new Iterator\Book("book3"));
+$bookShelf = new App\Design\Iterator\BookShelf(3);
+$bookShelf->appendBook(new App\Design\Iterator\Book("book1"));
+$bookShelf->appendBook(new App\Design\Iterator\Book("book2"));
+$bookShelf->appendBook(new App\Design\Iterator\Book("book3"));
 $iterator = $bookShelf->iterator();
 
 while ($iterator->hasNext()) {
     $book = $iterator->next();
-    print_r($book->getName());
+    echo $book->getName() . "\n";
 }
